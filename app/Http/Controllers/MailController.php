f37d526badf46bafb53b48b3fc29dd236e349747
objectisliper <objectisliper@gmail.com>
2019-05-17 00:37:35 +0300

    validation, captcha, etc

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -10,7 +10,14 @@ class MailController extends Controller
 {
     public function send(Request $request)
     {
-        Mail::send(['html'=>'mail'],['name' => $_POST['name'], 'phone' => $_POST['phone'], 'email' => $_POST['email'], 'text' => $_POST['text']], function ($message){
+        $this->validate($request, [
+            'name' => 'required',
+            'email' => 'required|email',
+            'phone' => 'required|regex: /(01)[0-9]{9}/',
+            'text' => 'required|min:10',
+            'g-recaptcha-response' => 'required|captcha',
+        ]);
+        Mail::send(['html'=>'mail'],['name' => $_POST['name'], 'phone' => $_POST['phone'], 'email' => $_POST['email'], 'text' => $_POST['text'], 'gRecaptchaResponse' => $_POST['g-recaptcha-response']], function ($message){
            $message->to('objectisliper@gmail.com', 'Site frixys')->subject('Test email');
            $message->from('site.frixys@gmail.com', 'Site frixys');
         });
--- resources/views/show_contact.blade.php
+++ resources/views/show_contact.blade.php
@@ -32,6 +32,14 @@
                 <p>Ваш телефон: <br> <input name="phone" class="form-control" required></p>
                 <p>Ваш e-mail: <br> <input name="email" class="form-control" required></p>
                 <p>Ваше сообщение: <br> <textarea name="text" class="form-control" required></textarea></p>
+                {!! app('captcha')->display() !!}
+                @if ($errors->has('g-recaptcha-response'))
+                    <span class="help-block">
+                                        <strong>{{ $errors->first('g-recaptcha-response') }}</strong>
+                                    </span>
+                @endif
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+                {!! NoCaptcha::renderJs() !!}
                 <button type="submit" class="btn-success">Отправить</button>
             </form>
         </div>
